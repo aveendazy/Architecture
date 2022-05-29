c1488c47d4da225fb0bfb57e46b75aa3f00b7542
aveendazy <zaveenda@mail.ugm.ac.id>
2022-05-29 18:30:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/architecture/services/DogService.java
@@ -0,0 +1,12 @@
+package com.example.architecture.services;
+
+import com.example.architecture.models.DogRandomResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface DogService {
+
+    @GET("breeds/image/random")
+    Call<DogRandomResponse> fetchRandomDog();
+}
